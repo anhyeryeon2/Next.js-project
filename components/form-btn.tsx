dffc9ffec0759ba3a8cf2c0f3c364a57fa6fa553
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-06-17 11:22:08 +0900

    6.0

--- components/form-btn.tsx
+++ components/form-btn.tsx
@@ -1,15 +1,20 @@
+"use client";
+import { useFormStatus } from "react-dom";
+
 interface FormButtonProps {
-    loading: boolean;
     text: string;
   }
   
-  export default function FormButton({ loading, text }: FormButtonProps) {
+  //hook으로 form의 로딩상태 알기 -> loading 삭제
+  export default function FormButton({ text }: FormButtonProps) {
+    const{pending} = useFormStatus();
+    //pending상태라면 버튼 비활성화 
     return (
       <button
-        disabled={loading}
+        disabled={pending}
         className="primary-btn h-10 disabled:bg-neutral-400  disabled:text-neutral-300 disabled:cursor-not-allowed"
       >
-        {loading ? "로딩 중" : text}
+        {pending ? "로딩 중" : text}
       </button>
     );
   }
