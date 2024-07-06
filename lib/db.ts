f927742f2959dde616aacee7da9079275cfc2a8a
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-07-06 23:46:46 +0900

    7.4

---
+++ lib/db.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+async function test(){
+ const token = await db.sMSToken.create({
+    data:{
+        token:"123123",
+        user:{
+            connect:{
+                id:3,
+            },
+        },
+    },
+ });
+ console.log(token);
+}
+test();
+export default db;
