17145af1cfcfaf9bda769f7e2c54ba962d9a0c48
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-07-22 21:56:02 +0900

    10.4

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,5 +1,9 @@
 import ListProduct from "@/components/list-product";
 import db from "@/lib/db";
+import { Prisma } from "@prisma/client";
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 async function getProducts() {
   const products = await db.product.findMany({
@@ -14,7 +18,17 @@ async function getProducts() {
   return products;
 }
 
+
 export default async function Products() {
+  // const product = await prisma.product.create({
+  //   data: {
+  //     title: "고구마",
+  //     price: 999,
+  //     photo: "/goguma.jpg",
+  //     description: "맛있는 고구마",
+  //     userId: 1,
+  //   },
+  // });
   const products = await getProducts();
   return (
     <div className="p-5 flex flex-col gap-5">
